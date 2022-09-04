1c0c6857e38b854eb981593272b5b530543f4640
ethanyuan223 <yicheng.yuan@mail.mcgill.ca>
2022-09-04 17:43:44 -0400

    dier

--- testproject/src/testproject/hello.java
+++ testproject/src/testproject/hello.java
@@ -1,5 +1,10 @@
 package testproject;
 
 public class hello {
+	public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 1;
+        int c = 1;
+    }
 
 }
